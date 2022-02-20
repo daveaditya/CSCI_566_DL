c0c4398738b039e5e93d842969befe128b87d775
Aditya Dave <aditya.dave2017@gmail.com>
2022-02-19 22:00:06 -0800

    done with problem 01 and working on SmallConvNet

---
+++ assignment_01/lib/cnn/cnn_models.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from lib.mlp.fully_conn import *
+from lib.mlp.layer_utils import *
+from lib.cnn.layer_utils import *
+
+
+""" Classes """
+class TestCNN(Module):
+    def __init__(self, dtype=np.float32, seed=None):
+        self.net = sequential(
+            ########## TODO: ##########
+            ConvLayer2D(3, 3, 3, name="conv"),
+            MaxPoolingLayer(2, 2, name="maxp"),
+            flatten(name="flatten"),
+            fc(27, 5, init_scale=0.02, name="fc")
+            ########### END ###########
+        )
+
+
+class SmallConvolutionalNetwork(Module):
+    def __init__(self, keep_prob=0, dtype=np.float32, seed=None):
+        self.net = sequential(
+            ########## TODO: ##########
+            ConvLayer2D(3, 5, 32, name="conv1"),
+            leaky_relu(name="lrelu1"),
+            MaxPoolingLayer(2, 2, name="maxp1"),
+            ConvLayer2D(32, 5, 64, name="conv2"),
+            leaky_relu(name="lrelu2"),
+            MaxPoolingLayer(2, 2, name="maxp2"),
+            flatten(name="flatten"),
+            leaky_relu(name="lrelu3"),
+            fc(1600, 10, name="fc1"),
+            dropout(keep_prob=0.25, name="dropout1")
+            ########### END ###########
+        )
