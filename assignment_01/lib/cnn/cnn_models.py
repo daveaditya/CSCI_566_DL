22e072debf03d021390a2db996357eb313be9fe5
Aditya Dave <aditya.dave2017@gmail.com>
2022-02-21 17:13:48 -0800

    new model at 0.822536

--- assignment_01/lib/cnn/cnn_models.py
+++ assignment_01/lib/cnn/cnn_models.py
@@ -20,17 +20,35 @@ class TestCNN(Module):
         )
 
 
+# class SmallConvolutionalNetwork(Module):
+#     def __init__(self, keep_prob=0, dtype=np.float32, seed=None):
+#         self.net = sequential(
+#             ########## TODO: ##########
+#             ConvLayer2D(3, 5, 8, 1, 1, name = "conv1"),
+#             leaky_relu(name="lr1"),
+#             ConvLayer2D(8, 6, 4, 1, 1, name = "conv2"),
+#             leaky_relu(name="lr2"),
+#             MaxPoolingLayer(3, 1, name = "pool2"),
+#             flatten(name = "flatten1"),
+#             fc(2500, 10, 0.02, name="fc1"),
+#             leaky_relu(name="lr3"),
+#             fc(10, 10, 0.02, name="fc2")
+#             ########### END ###########
+#         )
+
+
 class SmallConvolutionalNetwork(Module):
     def __init__(self, keep_prob=0, dtype=np.float32, seed=None):
         self.net = sequential(
             ########## TODO: ##########
-            ConvLayer2D(3, 5, 8, 1, 1, name = "conv1"),
+            ConvLayer2D(3, 3, 16, 1, 0, name = "conv1"),
             leaky_relu(name="lr1"),
-            ConvLayer2D(8, 6, 4, 1, 1, name = "conv2"),
+            MaxPoolingLayer(3, 2, name = "pool1"),
+            ConvLayer2D(16, 3, 8, 1, 0, name = "conv2"),
             leaky_relu(name="lr2"),
             MaxPoolingLayer(3, 1, name = "pool2"),
             flatten(name = "flatten1"),
-            fc(2500, 10, 0.02, name="fc1"),
+            fc(800, 10, 0.02, name="fc1"),
             leaky_relu(name="lr3"),
             fc(10, 10, 0.02, name="fc2")
             ########### END ###########
