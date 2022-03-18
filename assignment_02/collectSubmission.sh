1f57e72736ea0d9a1a91f5c36c4d05126e0348b9
Aditya Dave <aditya.dave2017@gmail.com>
2022-03-18 15:51:40 -0700

    correct assignment name

--- assignment_02/collectSubmission.sh
+++ assignment_02/collectSubmission.sh
@@ -1,2 +1,2 @@
-rm -f assignment1.zip 
-zip -r assignment1.zip . -i "*.ipynb" "problem_1_solution.pdf" "problem_2_solution.pdf" -x "*.ipynb_checkpoints*"
+rm -f assignment2.zip
+zip -r assignment2.zip . -i "*.ipynb" "problem_1_solution.pdf" "problem_2_solution.pdf" -x "*.ipynb_checkpoints*"
